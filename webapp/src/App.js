b5fb523842bafddc6ed2ebab8613cf4962a11575
Asim <uze761@gmail.com>
2022-04-09 16:12:38 +0530

    Added React App

---
+++ webapp/src/App.js
@@ -0,0 +1,13 @@
+import DataGrid from "./components/DataGrid";
+import  Header  from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <DataGrid />
+    </>
+  );
+}
+
+export default App;
